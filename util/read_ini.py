15286c1a84b99a50ef2a6e851ad0c2d0a4008be4
humiao159 <1160435259@qq.com>
2019-11-07 23:51:31 +0800

    haha

---
+++ util/read_ini.py
@@ -0,0 +1,8 @@
+# -  *  - coding:utf-8  -  *  -
+class Readini():
+    def __init__(self,filename=None):
+        if filename is None:
+            self.filename="../data/RegisterElement.ini"
+        else:
+            self.filename="../data/"+"filename"
+
